ef99c1e960b9be9ad98a2df5b2ee4c7795244660
Evgeny Grebnev <evg@wallter.com>
2018-12-04 18:44:02 +0200

    Translations added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="identity" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="identity" class="col-sm-4 col-form-label text-md-right">{{ __('Identity') }}</label>
 
                             <div class="col-md-6">
                                 <input id="identity" type="text" class="form-control{{ $errors->has('identity') ? ' is-invalid' : '' }}" name="identity" value="{{ old('identity') }}" required autofocus>
