146c311bac77e3b958d77b4aa4ab9c1665774c8c
Evgeny Grebnev <evg@wallter.com>
2018-12-24 21:42:33 +0200

    Translations.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -60,7 +60,7 @@
                 <button type="button" 
                     class="btn btn-default" 
                     data-dismiss="modal">
-                        Close
+                        {{ __('Close') }}
                 </button>
                 <span class="pull-right">
                     <button type="submit" class="btn btn-primary" id="send_time">
@@ -76,7 +76,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
