fddb350f8dc2a2dd596efc6f5f88ce5d3e94c191
Evgeny Grebnev <evg@wallter.com>
2019-01-02 08:46:00 +0200

    search cont

--- resources/views/admin/show_date.blade.php
+++ resources/views/admin/show_date.blade.php
@@ -5,6 +5,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+        <div class="col-md-12 mb-2">
+            <div class="card">
+                <div class="card-body">input search here</div>
+            </div>
+        </div>
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
