51e073e41e7a5d75cb54b0fedab164b9a6be09c8
Evgeny Grebnev <evg@wallter.com>
2019-01-02 08:43:01 +0200

    Start creating serch by identity

--- resources/views/admin/show_date.blade.php
+++ resources/views/admin/show_date.blade.php
@@ -17,7 +17,7 @@
                     @endif
                     
                     @foreach($lessons as $key => $lesson)
-                    <div class="row text-center border-bottom mb-2">
+                    <div class="row text-center border-bottom mb-2" data-identity="{{ $lesson!==null?$lesson->user->identity:'' }}">
                         <div class="col-3">
                             @if($lesson !== null)
                                 {{ Carbon\Carbon::parse($lesson->time)->format('H:i') }}
