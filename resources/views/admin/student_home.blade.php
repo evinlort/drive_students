a4ef483cb86242bb2073a749174390fb24e2de43
Evgeny Grebnev <evinlorth@gmail.com>
2020-05-06 22:46:22 +0300

    DS-7 Link to report from set lessons page

--- resources/views/admin/student_home.blade.php
+++ resources/views/admin/student_home.blade.php
@@ -123,6 +123,11 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-12 text-center">
+            <a class="btn btn-primary" href="{{ route('student_report').'?choosen_student='.$user->identity }}">Report</a>
+        </div>
+    </div>
 </div>
 </div>
 </div>
